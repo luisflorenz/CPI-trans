16fc214d98e9c56361e7463f6c4b25913a6e1e95
Luis Florenzan <86024563+luisflorenz@users.noreply.github.com>
2024-11-14 20:20:34 +0000

    commit aal the cargo.tml changes

---
+++ CPI-transfer/target/debug/deps/CPI_transfer-bdba39559f873041.d
@@ -0,0 +1,5 @@
+/workspaces/CPI-trans/CPI-transfer/target/debug/deps/CPI_transfer-bdba39559f873041: src/lib.rs
+
+/workspaces/CPI-trans/CPI-transfer/target/debug/deps/CPI_transfer-bdba39559f873041.d: src/lib.rs
+
+src/lib.rs:
